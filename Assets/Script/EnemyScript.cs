e9911a1fd87ca59d3804b72558bf2cddb8a22820
ryonakao <rodriguez.nak@gmail.com>
2016-06-19 18:21:46 +0900

    first commit
    初めてのコミット

---
+++ Assets/Script/EnemyScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour 
+{
+	public int enemyHP = 3;
+	public GameObject deathEffect;
+	public GameObject spark;
+	public Transform target;
+	NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //playerを追いかける
+		agent.SetDestination(target.position);
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "sumaho")
+		{
+			enemyHP -= 1;
+			Instantiate (spark, transform.position, Quaternion.identity);
+
+			if (enemyHP <= 0) 
+			{
+				//爆発する
+				Instantiate (deathEffect, transform.position, Quaternion.identity);
+				Destroy (this.gameObject);
+			}
+		}
+	}
+
+
+}
---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	 
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			animator.SetBool ("Attack2", true);
+
+		}
+		if (Input.GetKeyUp (KeyCode.UpArrow)) {
+			animator.SetBool ("Attack2", false);
+		}
+	//上の二つは後でスマホ用に変更する
+	}
+
+
+}
